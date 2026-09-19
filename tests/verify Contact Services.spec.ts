a16d2cfee748503aee2978036a382e309a0850bb
asyouknowvimalbind96 <ramvimal7800@gmail.com>
2026-09-19 11:44:44 +0530

    complete TestCases our According Practice

--- tests/verify Contact Services.spec.ts
+++ tests/verify Contact Services.spec.ts
@@ -3,10 +3,6 @@ import {  APIResponse, expect } from "@playwright/test";
 import testData from '../TestData/ContactsDataFormate.json'
 
 
-
-
-
-
 test('listing contacts',async({contacts})=>{
 let response:APIResponse=await contacts.getContacts("dev/api/contacts");
 let jsonResponse=await response.json();
@@ -14,13 +10,11 @@ console.log(jsonResponse);
 expect(response.status()).toBe(200);
 });
 
-
 let data=structuredClone(testData.Data.CreateContact);
 let emailProperties=data.properties.find(property=>property.name==="email");
 if(emailProperties){
 emailProperties.value="vimaly11@gmail.com";
 }
-
 
 test('verify Contact Creation',async({contacts})=>{
 let response:APIResponse=await contacts.createContacts("dev/api/contacts",data);
@@ -40,13 +34,10 @@ console.log(jsonBody)
 console.log(jsonBody.id);
 });
 
-
-
 let updateData=structuredClone(testData.Data.UpdateContact);
 updateData.tags[0]="QA Engneer";
 updateData.id="4730199113138176";
 updateData.properties[2]!.value="vimalb@gmail.com";
-
 
 test('update contacts with  data',async({contacts})=>{
 let response:APIResponse=await contacts.updateContactWithpartiallyData("dev/api/contacts/edit-properties/",updateData);
@@ -57,11 +48,9 @@ expect(jsonBody.properties[13].value).toBe("vimalb@gmail.com");
 expect(jsonBody.tags[0]).toBe("QA Engneer")
 })
 
-
 let scoreID= structuredClone(testData.Data.updateLeadScore_ById);
 scoreID.id="4730199113138176";
 scoreID.lead_score=101;
-
 
 test('Update lead score by ID',async({contacts})=>{
 let response:APIResponse=await contacts.updateLeadScoreById("dev/api/contacts/edit/lead-score",scoreID);
@@ -70,9 +59,7 @@ console.log(jsonBody);
 expect(response.status()).toBe(200);
 console.log(response.statusText());
 expect(jsonBody.id).toBe(4730199113138176);
-
 })
-
 
 let starValue=structuredClone(testData.Data.updateStarValueByID);
 starValue.id="4730199113138176",
@@ -96,26 +83,22 @@ let jsonBody=await response.json();
 expect(jsonBody.id).toBe(4730199113138176);
 expect(response.status()).toBe(200);
 console.log(jsonBody);
-
 })
 
 let tagJsonData=structuredClone(testData.Data.deleteTagsValue);
 tagJsonData.id="4730199113138176";
 tagJsonData.tags[0]="Lead";
 
-
 test('Delete tags value by ID',async({contacts})=>{
 let response=await contacts.deleteTagsValueByID("dev/api/contacts/delete/tags",tagJsonData);
 let jsonBody=await response.json();
 console.log(jsonBody);
 })
-
 test('Delete single contact',async({contacts})=>{
 let response:APIResponse=await contacts.deleteSingleContactByID("dev/api/contacts/5244611389489152");
 console.log(response.status());
 expect(response.status()).toBe(204);
 })
-
 test('Search contact by email',async({contacts})=>{
 let response:APIResponse=await contacts.searchContactByEmail("dev/api/contacts/search/email/vimaly11@gmail.com");
 let jsonBody=await response.json();
